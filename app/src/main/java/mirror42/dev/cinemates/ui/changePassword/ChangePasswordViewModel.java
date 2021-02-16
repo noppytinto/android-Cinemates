036a467da4e1f834cb30f334317cae088f946d9e
chrisIt97 <57763991+chrisIt97@users.noreply.github.com>
2021-02-16 19:50:21 +0100

    Add logic to change password

--- app/src/main/java/mirror42/dev/cinemates/ui/changePassword/ChangePasswordViewModel.java
+++ app/src/main/java/mirror42/dev/cinemates/ui/changePassword/ChangePasswordViewModel.java
@@ -1,8 +1,173 @@
 package mirror42.dev.cinemates.ui.changePassword;
 
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import mirror42.dev.cinemates.api.mailAPI.JavaMailAPI;
+import mirror42.dev.cinemates.model.User;
+import mirror42.dev.cinemates.ui.resetPassword.ResetPasswordViewModel;
+import mirror42.dev.cinemates.utilities.HttpUtilities;
+import mirror42.dev.cinemates.utilities.MyUtilities;
+import mirror42.dev.cinemates.utilities.OkHttpSingleton;
+import mirror42.dev.cinemates.utilities.RemoteConfigServer;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class ChangePasswordViewModel extends ViewModel {
+
+    private final String TAG = this.getClass().getSimpleName();
+    private MutableLiveData<ChangePasswordViewModel.ResetResult> resetResult;
+    private RemoteConfigServer remoteConfigServer;
+
+    public enum ResetResult {
+        FAILED,
+        SUCCESS,
+        NONE
+    }
+    public ChangePasswordViewModel(){
+        resetResult = new MutableLiveData<>(ResetResult.NONE);
+        remoteConfigServer = RemoteConfigServer.getInstance();
+    }
+
+    // getter and setter
+    public void postResetStatus(ResetResult resetResult) {
+        this.resetResult.postValue(resetResult);
+    }
+
+    public LiveData<ResetResult> getResetStatus() {
+        return resetResult;
+    }
+
+
+    public void changePassword(User userLogged, String password){
+        final OkHttpClient httpClient = OkHttpSingleton.getClient();
+        final String dbFunction = "fn_update_password";
+        final String passwordHide = MyUtilities.SHA256encrypt(password);
+        try{
+            HttpUrl httpUrl = new HttpUrl.Builder()
+                    .scheme("https")
+                    .host(remoteConfigServer.getAzureHostName())
+                    .addPathSegments(remoteConfigServer.getPostgrestPath())
+                    .addPathSegment(dbFunction)
+                    .build();
+            RequestBody requestBody = new FormBody.Builder()
+                    .add("mail",userLogged.getEmail())
+                    .add("newpassword",passwordHide)
+                    .add("typeupdate","CHANGE")
+                    .build();
+            Request request = HttpUtilities.buildPostgresPOSTrequest(httpUrl,requestBody,userLogged.getAccessToken());
+
+            Call call = httpClient.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    Log.v(TAG,"cambio password fallito");
+                    postResetStatus(ResetResult.FAILED);
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                    try{
+                        Log.v(TAG,"response :" + response);
+                        if(response.isSuccessful()) {
+                            String responseData = response.body().toString();
+                            Log.v(TAG,"cambio avvenuto");
+                            updateToken(userLogged);
+                        }else{
+                            Log.v(TAG,"mail non trovata ");
+                            postResetStatus(ResetResult.FAILED);
+                        }
+
+
+                    }catch(Exception e){
+                        e.printStackTrace();
+                        postResetStatus(ResetResult.FAILED);
+                    }
+
+                }
+            });
+        }catch(Exception e){
+            e.printStackTrace();
+            postResetStatus(ResetResult.FAILED);
+        }
+    }
+
+    private void updateToken(User userLogged){
+        final OkHttpClient httpClient = OkHttpSingleton.getClient();
+        final String dbFunction = "fn_select_accesstoken";
+        try{
+            HttpUrl httpUrl = new HttpUrl.Builder()
+                    .scheme("https")
+                    .host(remoteConfigServer.getAzureHostName())
+                    .addPathSegments(remoteConfigServer.getPostgrestPath())
+                    .addPathSegment(dbFunction)
+                    .build();
+            RequestBody requestBody = new FormBody.Builder()
+                    .add("mail",userLogged.getEmail())
+                    .build();
+            Request request = HttpUtilities.buildPostgresPOSTrequest(httpUrl,requestBody,remoteConfigServer.getGuestToken());
+
+            Call call = httpClient.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    Log.v(TAG,"recupero nuovo access token fallito");
+                    postResetStatus(ResetResult.FAILED);
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                    try{
+                        Log.v(TAG,"response :" + response);
+                        if(response.isSuccessful()) {
+
+                            String responseData = response.body().string();
+
+                            if( ! responseData.equals("null")) {
+                                JSONObject jsonObject = new JSONObject(responseData);
+                                String accessToken = jsonObject.getString("AccessToken");
+                                Log.v(TAG, "access token recuperato  " + accessToken);
+                                userLogged.setAccessToken(accessToken);
+                                postResetStatus(ResetResult.SUCCESS);
+                            }else{
+                                Log.v(TAG,"errore recupero access token ");
+                                postResetStatus(ResetResult.FAILED);
+                            }
+                        }else{
+                            Log.v(TAG,"errore recupero access token ");
+                            postResetStatus(ResetResult.FAILED);
+                        }
+
+
+                    }catch(Exception e){
+                        e.printStackTrace();
+                        postResetStatus(ResetResult.FAILED);
+                    }
+
+                }
+            });
+        }catch(Exception e){
+            e.printStackTrace();
+            postResetStatus(ResetResult.FAILED);
+        }
+    }
 
 
 
--- app/src/main/java/mirror42/dev/cinemates/ui/changePassword/changePasswordFragment.java
+++ app/src/main/java/mirror42/dev/cinemates/ui/changePassword/changePasswordFragment.java
@@ -2,36 +2,53 @@ package mirror42.dev.cinemates.ui.changePassword;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import mirror42.dev.cinemates.R;
+import mirror42.dev.cinemates.model.User;
 import mirror42.dev.cinemates.ui.login.LoginViewModel;
+import mirror42.dev.cinemates.ui.resetPassword.ResetPasswordViewModel;
+import mirror42.dev.cinemates.utilities.FirebaseAnalytics;
+import mirror42.dev.cinemates.utilities.MyUtilities;
 
 
 public class changePasswordFragment extends Fragment implements
         View.OnClickListener{
 
     private final String TAG = getClass().getSimpleName();
+
+
+    private TextInputLayout textInputLayoutActualPassword;
+    private TextInputEditText editTextActualPassword;
+
+    private TextInputLayout textInputLayoutNewPassword;
+    private TextInputEditText editTextNewPassword;
+
+    private TextInputLayout textInputLayoutRepPassword;
+    private TextInputEditText editTextRepPassword;
+
     private Button changePassword;
-    private LoginViewModel loginViewModel;
+
     private View view;
 
-    public changePasswordFragment() {
+    private ChangePasswordViewModel changePasswordViewModel;
+    private LoginViewModel loginViewModel;
 
-    }
-
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private FirebaseAnalytics firebaseAnalytics;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,8 +57,131 @@ public class changePasswordFragment extends Fragment implements
         return inflater.inflate(R.layout.fragment_change_password, container, false);
     }
 
-    @Override
-    public void onClick(View v) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // firebase logging
+        firebaseAnalytics = FirebaseAnalytics.getInstance();
+        firebaseAnalytics.logScreenEvent(this, getString(R.string.changePassword_page_firebase_changePassword), getContext());
+
+        initViews(view);
+
+        loginViewModel = new ViewModelProvider(requireActivity()).get(LoginViewModel.class);
+
+        changePasswordViewModel =  new ViewModelProvider(this).get(ChangePasswordViewModel.class);
+        changePasswordViewModel.getResetStatus().observe(getViewLifecycleOwner(), resetResult -> {
+            if(resetResult == ChangePasswordViewModel.ResetResult.SUCCESS)
+                showCenteredToast( "Cambio password completo ");
+            else if(resetResult == ChangePasswordViewModel.ResetResult.FAILED)
+                showCenteredToast( "Cambio password fallito ");
+        });
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == changePassword.getId()){
+
+            String actualPass =   editTextActualPassword.getText().toString();
+            String newPass =   editTextNewPassword.getText().toString();
+            String RepNewPass =   editTextRepPassword.getText().toString();
+
+            // metti tra i due checkActualPasswordMatch()!!
+            if(checkAllFieldsAreFilled() && checkRepeatPasswordMatch()){
+                changePasswordViewModel.changePassword(loginViewModel.getLoggedUser(), newPass);
+            }else{
+                showCenteredToast("Completare prima tutti i campi evidenziati in rosso.");
+            }
+        }
+    }
+
+
+
+
+    private void initViews(View view){
+        this.view = view;
+
+        textInputLayoutActualPassword = view.findViewById(R.id.textInputLayout_changePasswordFragment_actualPassword);
+        editTextActualPassword = view.findViewById(R.id.editText_changePasswordFragment_actualPassword);
+
+        textInputLayoutNewPassword = view.findViewById(R.id.textInputLayout_changePasswordFragment_newPassword);
+        editTextNewPassword = view.findViewById(R.id.editText_changePassword_newPassword);
+
+        textInputLayoutRepPassword = view.findViewById(R.id.textInputLayout_changePasswordFragment_newPasswordRep);
+        editTextRepPassword = view.findViewById(R.id.editText_changePassword_newPasswordRep);
+
+        changePassword = view.findViewById(R.id.button_changePasswordFragment_startChangePassword);
+        changePassword.setOnClickListener(this);
+
+    }
+
+
+    private boolean checkAllFieldsAreFilled() {
+        boolean res = true;
+
+        if(editTextActualPassword.getText().toString().isEmpty()) {
+            textInputLayoutActualPassword.setError("*");
+            res = false;
+        }
+        else {
+            textInputLayoutActualPassword.setError(null);
+        }
+
+        if(editTextNewPassword.getText().toString().isEmpty()) {
+            textInputLayoutNewPassword.setError("*");
+            res = false;
+        }
+        else {
+            textInputLayoutNewPassword.setError(null);
+        }
+
+        if(editTextRepPassword.getText().toString().isEmpty()) {
+            textInputLayoutRepPassword.setError("*");
+            res = false;
+        }
+        else {
+            textInputLayoutRepPassword.setError(null);
+        }
+
+        return res;
+    }
+
+    private boolean checkRepeatPasswordMatch() {
+
+        if( ! editTextRepPassword.getText().toString().equals(editTextNewPassword.getText().toString())) {
+            textInputLayoutRepPassword.setError(getString(R.string.passwords_dont_match));
+            return false;
+        }
+        else {
+            textInputLayoutRepPassword.setError(null);
+        }
+
+        return true;
+    }
+
+    private boolean checkActualPasswordMatch() {
+
+        final String passwordHide = MyUtilities.SHA256encrypt(editTextActualPassword.getText().toString());
+
+        if( ! passwordHide.equals(loginViewModel.getLoggedUser().getPassword())) {
+            textInputLayoutActualPassword.setError(getString(R.string.passwords_dont_match));
+            return false;
+        }
+        else {
+            textInputLayoutActualPassword.setError(null);
+        }
+
+        return true;
+    }
+
+
+
+    public void showCenteredToast(String message) {
+        final Toast toast = Toast.makeText(getContext(), message, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+
+
 }
