6883bb7ced86978e0609afd3e094c970459a7846
chrisIt97 <57763991+chrisIt97@users.noreply.github.com>
2021-02-27 15:06:34 +0100

    Add login with api google

---
+++ app/src/main/java/mirror42/dev/cinemates/ui/login/GoogleLoginFragment.java
@@ -0,0 +1,32 @@
+package mirror42.dev.cinemates.ui.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mirror42.dev.cinemates.R;
+
+public class GoogleLoginFragment extends Fragment {
+
+
+
+    public GoogleLoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_google_login, container, false);
+    }
+}
