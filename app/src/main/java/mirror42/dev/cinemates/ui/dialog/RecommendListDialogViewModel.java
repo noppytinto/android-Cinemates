b4d0487dc778020894a8d611cf43e65ba0346828
chrisIt97 <57763991+chrisIt97@users.noreply.github.com>
2021-03-03 10:47:41 +0100

    Fix dialog recommendList external user image

--- app/src/main/java/mirror42/dev/cinemates/ui/dialog/RecommendListDialogViewModel.java
+++ app/src/main/java/mirror42/dev/cinemates/ui/dialog/RecommendListDialogViewModel.java
@@ -152,7 +152,11 @@ public class RecommendListDialogViewModel extends ViewModel {
         user.setUsername(jsonObject.getString("Username"));
         user.setFirstName(jsonObject.getString("Name"));
         user.setLastName(jsonObject.getString("LastName"));
-        user.setProfilePictureURL(remoteConfigServer.getCloudinaryDownloadBaseUrl() + jsonObject.getString("ProfileImage"));
+       user.setExternalUser(jsonObject.getBoolean("ExternalAccount"));
+        if(user.getIsExternalUser())
+            user.setProfilePictureURL(jsonObject.getString("ProfileImage"));
+        else
+            user.setProfilePictureURL(remoteConfigServer.getCloudinaryDownloadBaseUrl() + jsonObject.getString("ProfileImage"));
         return user;
     }
 
